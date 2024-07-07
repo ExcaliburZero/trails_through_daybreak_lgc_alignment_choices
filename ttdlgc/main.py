0cce96cb1750114c5285f5a585dd938b9d0fca91
Christopher Wells <cwellsny@nycap.rr.com>
2024-07-07 02:04:53 -0700

    Break into two separate subcommands

--- ttdlgc/main.py
+++ ttdlgc/main.py
@@ -1,4 +1,4 @@
-from typing import Optional
+from typing import Any, Optional
 
 import logging
 import pathlib
@@ -12,11 +12,13 @@ from .simulation import Simulation
 SUCCESS = 0
 _FAILURE = 1
 
+app = typer.Typer()
 
-def main(
+
+@app.command()
+def solve(
     events_filepath: pathlib.Path,
     output_solution_filepath: Optional[pathlib.Path] = None,
-    input_solution_filepath: Optional[pathlib.Path] = None,
     constraint: Optional[list[Constraint]] = None,
     verbose: bool = False,
 ) -> int:
@@ -28,31 +30,26 @@ def main(
     with open(events_filepath, "r", encoding="utf-8") as input_stream:
         events = Event.multiple_from_csv(input_stream)
 
-    if input_solution_filepath is None:
-        if constraint is None:
-            constraint = []
+    if constraint is None:
+        constraint = []
 
-        problem = create_milp(events, constraint)
-        logging.debug(problem)
+    problem = create_milp(events, constraint)
+    logging.debug(problem)
 
-        problem.solve()
-        logging.debug("==============")
-        logging.debug("Solution:")
-        for vairable in problem.variables():
-            logging.debug(f"{vairable} = {vairable.varValue}")
+    problem.solve()
+    logging.debug("==============")
+    logging.debug("Solution:")
+    for vairable in problem.variables():
+        logging.debug(f"{vairable} = {vairable.varValue}")
 
-        solution = extract_solution(events, problem)
-        logging.info("===============")
-        logging.info("Choices:")
-        logging.info(f"\tChapter 5 Route: {solution.route}")
-        for event, choice_index in solution.choices:
-            logging.info(
-                f"\t{event.name} chose <{event.choices[choice_index].name}> ({choice_index}) ({event.choices[choice_index].impact})"
-            )
-    else:
-        with open(input_solution_filepath, "r", encoding="utf8") as input_stream:
-            solution = Solution.from_csv(events, input_stream)
-        logging.info(f"Loaded solution from: {input_solution_filepath}")
+    solution = extract_solution(events, problem)
+    logging.info("===============")
+    logging.info("Choices:")
+    logging.info(f"\tChapter 5 Route: {solution.route}")
+    for event, choice_index in solution.choices:
+        logging.info(
+            f"\t{event.name} chose <{event.choices[choice_index].name}> ({choice_index}) ({event.choices[choice_index].impact})"
+        )
 
     if output_solution_filepath is not None:
         with open(output_solution_filepath, "w", encoding="utf8") as output_stream:
@@ -67,5 +64,31 @@ def main(
     return SUCCESS
 
 
-def main_without_args() -> None:
-    return typer.run(main)
+@app.command()
+def simulate(
+    events_filepath: pathlib.Path,
+    input_solution_filepath: pathlib.Path,
+    verbose: bool = False,
+) -> int:
+    logging.basicConfig(
+        level=logging.DEBUG if verbose else logging.INFO,
+        format="%(levelname)s> %(message)s",
+    )
+
+    with open(events_filepath, "r", encoding="utf-8") as input_stream:
+        events = Event.multiple_from_csv(input_stream)
+
+    with open(input_solution_filepath, "r", encoding="utf8") as input_stream:
+        solution = Solution.from_csv(events, input_stream)
+    logging.info(f"Loaded solution from: {input_solution_filepath}")
+
+    simulation = Simulation(events)
+    simulation.apply_solution(solution)
+
+    logging.info(f"Final LGC = {simulation.lgc}")
+
+    return SUCCESS
+
+
+def main_without_args() -> Any:
+    return app()
